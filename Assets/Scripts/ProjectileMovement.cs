70809a2de868284233dd791fbf0dad866d29703f
Yann <nayplatypusmith@live.fr>
2016-10-19 01:08:55 +0200

    Tests Projectiles
    Ajout SpawnManager
    Création de projectiles

---
+++ Assets/Scripts/ProjectileMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileMovement : MonoBehaviour
+{
+	private Rigidbody ProjectileBody;
+	public float movementSpeed;
+
+	void Start()
+	{
+		ProjectileBody = GetComponent <Rigidbody> ();
+		//ProjectileBody.velocity = transform.forward * movementSpeed;
+		ProjectileBody.MovePosition (transform.position + new Vector3(movementSpeed * Time.deltaTime, 0, 0));
+	}
+}
