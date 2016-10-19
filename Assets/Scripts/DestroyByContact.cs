ae4d5ea00d10d916967e27232b5148bac658d0c2
Ysnir <nayplatypusmith@live.fr>
2016-10-19 16:27:55 +0200

    Debug Collision

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -2,15 +2,20 @@
 using System.Collections;
 
 public class DestroyByContact : MonoBehaviour {
+	private static int nbCollision = 0;
 
 	void OnTriggerEnter(Collider other)
 	{
 		switch (other.tag) {
 		case "Poui":
 			Destroy (gameObject);
+			nbCollision++;
+			Debug.Log ("Collision avec Poui, nombre collision : " + nbCollision);
 			break;
 		case "Player":
 			Destroy (gameObject);
+			nbCollision++;
+			Debug.Log ("Collision avec Player, nombre collision : " + nbCollision);
 			break;
 		}
 	}
